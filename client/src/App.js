53a5896e5e69e0a778344411d311a914448d66ce
Durga Potnuru <vijaypotnuru123@gmail.com>
2023-09-11 20:51:48 +0530

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-green-300">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
